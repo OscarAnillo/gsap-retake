6f2ccaac4ae903dd84ff3d7bf3e7faa176c9350e
oscar anillo <oscarani14@gmail.com>
2022-11-11 22:16:14 -0500

    add gsap and a basic tween

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useEffect, useRef } from 'react';
+import {gsap} from 'gsap';
 
 import './App.css';
 
 function App() {
+  const nameRef = useRef(null);
+  const positionRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(".name", {
+      opacity: 0,
+      duration: 5,
+      y: -600,
+      fontSize: "9rem",
+      ease: 'elastic',
+    })
+    gsap.to(".name", {
+      opacity: 1,
+      duration: 5,
+      fontSize: "9rem",
+      y: 350,
+      ease: 'elastic',
+    })
+    gsap.from(".position", {
+      opacity: 0,
+      duration: 5,
+      y: -100,
+      fontSize: "9rem",
+      ease: 'elastic',
+    })
+    gsap.to(".position", {
+      opacity: 1,
+      duration: 5,
+      fontSize: "9rem",
+      y: 300,
+      ease: 'elastic',
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1>Oscar Anillo</h1>
+      <h1 className='name' ref={nameRef}>Oscar Anillo</h1>
+      <p className='position' ref={positionRef}>Sr. Fullstack Developer</p>
     </div>
   );
 }
